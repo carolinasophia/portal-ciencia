813c56d0e82b32e6cf12e518c997feac72021522
Sophia Carolina Tavares Ribeiro <00001129962830sp@aluno.educacao.sp.gov.br>
2026-05-25 08:11:18 -0300

    Save changes

--- js/busca.js
+++ js/busca.js
@@ -1,23 +1,31 @@
 const barraBusca = document.getElementById("barraBusca");
 const cards = document.querySelectorAll(".card");
+const mensagemErro = document.getElementById("mensagemErro");
 
 barraBusca.addEventListener("input", () => {
+
+    const valor = barraBusca.value.toLowerCase();
 
     const pesquisa = barraBusca.value
         .toLowerCase()
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "");
 
+    let encontrou = false;
+
     cards.forEach(card => {
+
+        const tituloSimples = card.dataset.titulo.toLowerCase();
 
         const titulo = card.dataset.titulo
             .toLowerCase()
             .normalize("NFD")
             .replace(/[\u0300-\u036f]/g, "");
 
-        if (titulo.includes(pesquisa)) {
+        if (titulo.includes(pesquisa) || tituloSimples.includes(valor)) {
 
             card.style.display = "";
+            encontrou = true;
 
         } else {
 
@@ -26,5 +34,15 @@ barraBusca.addEventListener("input", () => {
         }
 
     });
+
+    if (encontrou) {
+
+        mensagemErro.style.display = "none";
+
+    } else {
+
+        mensagemErro.style.display = "block";
+
+    }
 
 });
